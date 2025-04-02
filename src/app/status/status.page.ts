d46d03af8fdec651c7c5680dbe53a79f90265f7e
unknown <loughnanefionn@gmail.com>
2025-04-02 14:37:49 +0000

    lab82

--- src/app/status/status.page.ts
+++ src/app/status/status.page.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonList, IonItem, IonLabel, IonRadioGroup, IonRadio, IonButton } from '@ionic/angular/standalone';
-import { Storage} from '@ionic/storage-angular';
+import { Storage} from '@ionic/storage-angular'; //Import IOnic storage - save data
 import { Router } from '@angular/router';
 
 @Component({
@@ -13,13 +13,14 @@ import { Router } from '@angular/router';
   imports: [IonButton, IonItem, IonList, IonButtons, IonBackButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel, IonRadioGroup, IonRadio]
 })
 export class StatusPage implements OnInit {
-  status:string=""
+  status:string="" //variable to store status value
   constructor(private storage:Storage, private router:Router) { }
 
+  //Method for onButtonClicked
   async onButtonClick() {
     console.log(this.status);
-    await this.storage.create();
-    await this.storage.set('status', this.status);
+    await this.storage.create(); //Initialize IOnic Storage 
+    await this.storage.set('status', this.status); //save current status value in storage
     // This makes the save button automatically send you back to home page
     this.router.navigate(['/home'])
   }
@@ -27,7 +28,8 @@ export class StatusPage implements OnInit {
   async ionViewWillEnter() {
     //Waits for storage to be saved so you dont outpace the code
     await this.storage.create();
-    this.status = await this.storage.get('status');
+    //retrive saved status from storage and assign  it to the status variable
+    this.status = await this.storage.get('status'); 
 
   }
 
